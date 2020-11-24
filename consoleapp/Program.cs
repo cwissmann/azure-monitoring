90eea36badc37f34cdeec7635666658e17fae481
Christian Wissmann <christian.wissmann@zeiss.com>
2020-11-24 13:16:47 +0100

    add console application showcase

---
+++ consoleapp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Schau mir in die Augen!");
+
+            var config = TelemetryConfiguration.CreateDefault();
+            config.InstrumentationKey = "INSTRUMENTATIONKEY";
+            var tc = new TelemetryClient(config);
+
+            // Track traces
+            tc.TrackTrace("Campus Console Trace", SeverityLevel.Information);
+
+            tc.Flush();
+        }
+    }
+}
